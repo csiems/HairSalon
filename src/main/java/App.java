a1089f6f77436988b01011ca98365eb442e5069f
Chris Siems <csiems@gmail.com>
2016-01-22 10:03:57 -0800

    create ability to add stylist to index page

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,5 +1,4 @@
 import java.lang.*;
-import java.util.ArrayList;
 import java.util.HashMap;
 import spark.ModelAndView;
 import spark.template.velocity.VelocityTemplateEngine;
@@ -12,6 +11,21 @@ public class App {
 
     get("/", (request, response) -> {
       HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    get("/newstylist", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/newstylist.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/", (request, response) -> {
+      HashMap<String, Object> model = new HashMap<String, Object>();
+      Stylist newStylist = new Stylist(request.queryParams("newStylistName"));
+      newStylist.save();
+      model.put("stylists", Stylist.all());
       model.put("template", "templates/index.vtl");
       return new ModelAndView(model, layout);
     }, new VelocityTemplateEngine());
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -31,5 +31,54 @@ public class AppTest extends FluentTest {
       assertThat(pageSource()).contains("Hair Salon");
     }
 
+    @Test
+     public void stylistIsCreatedTest() {
+       goTo("http://localhost:4567/");
+       click("a", withText("Add A New Stylist"));
+       fill("#newStylistName").with("Paul Mitchell");
+       submit(".btn");
+       assertThat(pageSource()).contains("Paul Mitchell");
+     }
+
+    //  @Test
+    //  public void multipleCategoriesAreCreated() {
+    //    Category myCategory = new Category("Household chores");
+    //    myCategory.save();
+    //    Category mySecondCategory = new Category("Grocery shopping");
+    //    mySecondCategory.save();
+    //    goTo("http://localhost:4567/categories");
+    //    assertThat(pageSource()).contains("Household chores");
+    //    assertThat(pageSource()).contains("Grocery shopping");
+    //  }
+     //
+    //  @Test
+    //  public void taskIsCreatedTest() {
+    //    goTo("http://localhost:4567/");
+    //    click("a", withText("Add a New Category"));
+    //    fill("#name").with("Household chores");
+    //    submit(".btn");
+    //    click("a", withText("Household chores"));
+    //    click("a", withText("Add a new task"));
+    //    fill("#description").with("Sweep floor");
+    //    submit(".btn");
+    //    assertThat(pageSource()).contains("Sweep floor");
+    //  }
+     //
+    //  @Test
+    //  public void multipleTasksAreCreatedTest() {
+    //    goTo("http://localhost:4567/");
+    //    click("a", withText("Add a New Category"));
+    //    fill("#name").with("Household chores");
+    //    submit(".btn");
+    //    click("a", withText("Household chores"));
+    //    click("a", withText("Add a new task"));
+    //    fill("#description").with("Sweep floor");
+    //    submit(".btn");
+    //    click("a", withText("Add a new task"));
+    //    fill("#description").with("Vacuum the rug");
+    //    submit(".btn");
+    //    assertThat(pageSource()).contains("Sweep floor");
+    //    assertThat(pageSource()).contains("Vacuum the rug");
+    //  }
 
 }
