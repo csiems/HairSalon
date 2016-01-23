4262e22bda2948b0de017f68f709d4c1fe6bff3f
Chris Siems <csiems@gmail.com>
2016-01-22 16:37:37 -0800

    complete update and delete integration tests

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -31,49 +31,96 @@ public class AppTest extends FluentTest {
       assertThat(pageSource()).contains("Hair Salon");
     }
 
-    @Test
-     public void stylistIsCreatedTest() {
-       goTo("http://localhost:4567/");
-       click("a", withText("Add A New Stylist"));
-       fill("#newStylistName").with("Paul Mitchell");
-       submit(".btn");
-       assertThat(pageSource()).contains("Paul Mitchell");
-     }
+  @Test
+   public void stylistIsCreatedTest() {
+     goTo("http://localhost:4567/");
+     click("a", withText("Add A New Stylist"));
+     fill("#newStylistName").with("Paul Mitchell");
+     submit(".btn");
+     assertThat(pageSource()).contains("Paul Mitchell");
+   }
 
-     @Test
-     public void multipleStylistsAreCreated() {
-       Stylist myStylist = new Stylist("Paul Mitchell");
-       myStylist.save();
-       Stylist mySecondStylist = new Stylist("José Eber");
-       mySecondStylist.save();
-       goTo("http://localhost:4567/");
-       assertThat(pageSource()).contains("Paul Mitchell");
-       assertThat(pageSource()).contains("José Eber");
-     }
+  @Test
+  public void multipleStylistsAreCreated() {
+    Stylist myStylist = new Stylist("Paul Mitchell");
+    myStylist.save();
+    Stylist mySecondStylist = new Stylist("José Eber");
+    mySecondStylist.save();
+    goTo("http://localhost:4567/");
+    assertThat(pageSource()).contains("Paul Mitchell");
+    assertThat(pageSource()).contains("José Eber");
+  }
 
-     @Test
-     public void clientIsCreatedTest() {
-       Stylist myStylist = new Stylist("Paul Mitchell");
-       myStylist.save();
-       goTo("http://localhost:4567/");
-       click("a", withText("Paul Mitchell"));
-       click("a", withText("Make An Appointment"));
-       fill("#newClientName").with("Khloe Kardashian");
-       submit(".btn");
-       assertThat(pageSource()).contains("Khloe Kardashian");
-     }
+  @Test
+  public void clientIsCreatedTest() {
+    Stylist myStylist = new Stylist("Paul Mitchell");
+    myStylist.save();
+    goTo("http://localhost:4567/");
+    click("a", withText("Paul Mitchell"));
+    click("a", withText("Make An Appointment With Paul Mitchell"));
+    fill("#newClientName").with("Khloe Kardashian");
+    submit(".btn");
+    assertThat(pageSource()).contains("Khloe Kardashian");
+  }
 
-     @Test
-     public void multipleTasksAreCreatedTest() {
-       Stylist myStylist = new Stylist("Paul Mitchell");
-       myStylist.save();
-       Client firstClient = new Client("Khloe Kardashian", myStylist.getId());
-       firstClient.save();
-       Client secondClient = new Client("Kim Kardashian", myStylist.getId());
-       secondClient.save();
-       goTo("http://localhost:4567/" + myStylist.getId());
-       assertThat(pageSource()).contains("Khloe Kardashian");
-       assertThat(pageSource()).contains("Kim Kardashian");
-     }
+  @Test
+  public void multipleClientsAreCreatedTest() {
+    Stylist myStylist = new Stylist("Paul Mitchell");
+    myStylist.save();
+    Client firstClient = new Client("Khloe Kardashian", myStylist.getId());
+    firstClient.save();
+    Client secondClient = new Client("Kim Kardashian", myStylist.getId());
+    secondClient.save();
+    goTo("http://localhost:4567/" + myStylist.getId());
+    assertThat(pageSource()).contains("Khloe Kardashian");
+    assertThat(pageSource()).contains("Kim Kardashian");
+  }
+
+  @Test
+  public void stylistIsUpdated() {
+    Stylist myStylist = new Stylist("Paul Mitchell");
+    myStylist.save();
+    goTo("http://localhost:4567/" + myStylist.getId());
+    fill("#updatedName").with("José Eber");
+    submit(".update");
+    goTo("http://localhost:4567/" + myStylist.getId());
+    assertThat(pageSource()).contains("José Eber");
+  }
+
+  @Test
+  public void stylistIsDeleted() {
+    Stylist myStylist = new Stylist("Paul Mitchell");
+    myStylist.save();
+    goTo("http://localhost:4567/" + myStylist.getId());
+    submit(".delete");
+    assertThat(pageSource()).contains("Successfully deleted!");
+  }
+
+  @Test
+  public void clientIsDeleted() {
+    Stylist myStylist = new Stylist("Paul Mitchell");
+    myStylist.save();
+    Client firstClient = new Client("Khloe Kardashian", myStylist.getId());
+    firstClient.save();
+    goTo("http://localhost:4567/" + myStylist.getId());
+    click("a", withText("Alter Record"));
+    submit(".delete");
+    goTo("http://localhost:4567/" + myStylist.getId());
+    assertThat(pageSource()).contains("No clients assigned.");
+  }
+
+  @Test
+  public void clientIsUpdated() {
+    Stylist myStylist = new Stylist("Paul Mitchell");
+    myStylist.save();
+    Client firstClient = new Client("Khloe Kardashian", myStylist.getId());
+    firstClient.save();
+    goTo("http://localhost:4567/" + myStylist.getId());
+    click("a", withText("Alter Record"));
+    fill("#updatedName").with("Kim Kardashian");
+    submit(".update");
+    goTo("http://localhost:4567/" + myStylist.getId());
+    assertThat(pageSource()).contains("Kim Kardashian");
+  }
 
 }
